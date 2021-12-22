4067e4e2a6bd9b0d27a60720b5594d812c0dd0f0
ZHI <891023883@qq.com>
2021-12-22 21:55:33 +0800

    手机登录，全局监听事件，

--- pages/hospital/detail/_hoscode.vue
+++ pages/hospital/detail/_hoscode.vue
@@ -3,20 +3,44 @@
   <div class="nav-container page-component">
     <!--左侧导航 #start -->
     <div class="nav left-nav">
-      <div class="nav-item ">
-        <span class="v-link clickable dark" :onclick="'javascript:window.location=\'/hospital/'+hospital.hoscode+'\''">预约挂号 </span>
+      <div class="nav-item">
+        <span
+          class="v-link clickable dark"
+          :onclick="
+            'javascript:window.location=\'/hospital/' + hospital.hoscode + '\''
+          "
+          >预约挂号
+        </span>
       </div>
       <div class="nav-item selected">
-        <span class="v-link selected dark" :onclick="'javascript:window.location=\'/hospital/detail/'+hospital.hoscode+'\''"> 医院详情 </span>
+        <span
+          class="v-link selected dark"
+          :onclick="
+            'javascript:window.location=\'/hospital/detail/' +
+            hospital.hoscode +
+            '\''
+          "
+        >
+          医院详情
+        </span>
       </div>
       <div class="nav-item">
-        <span class="v-link clickable dark" :onclick="'javascript:window.location=\'/hospital/notice/'+hospital.hoscode+'\''"> 预约须知 </span>
+        <span
+          class="v-link clickable dark"
+          :onclick="
+            'javascript:window.location=\'/hospital/notice/' +
+            hospital.hoscode +
+            '\''
+          "
+        >
+          预约须知
+        </span>
       </div>
-      <div class="nav-item "><span
-        class="v-link clickable dark"> 停诊信息 </span>
+      <div class="nav-item">
+        <span class="v-link clickable dark"> 停诊信息 </span>
       </div>
-      <div class="nav-item "><span
-        class="v-link clickable dark"> 查询/取消 </span>
+      <div class="nav-item">
+        <span class="v-link clickable dark"> 查询/取消 </span>
       </div>
     </div>
     <!-- 左侧导航 #end -->
@@ -25,24 +49,37 @@
     <div class="page-container">
       <div class="hospital-detail">
         <div class="common-header">
-          <div class="title-wrapper"><span class="hospital-title">{{ hospital.hosname }}</span>
-            <div class="icon-wrapper"><span class="iconfont"></span> {{ hospital.param.hostypeString }}</div>
+          <div class="title-wrapper">
+            <span class="hospital-title">{{ hospital.hosname }}</span>
+            <div class="icon-wrapper">
+              <span class="iconfont"></span> {{ hospital.param.hostypeString }}
+            </div>
           </div>
         </div>
-        <div class="info-wrapper"><img :src="'data:image/jpeg;base64,'+hospital.logoData" :alt="hospital.hosname" style="width: 80px; height: 80px;">
+        <div class="info-wrapper">
+          <img
+            :src="'data:image/jpeg;base64,' + hospital.logoData"
+            :alt="hospital.hosname"
+            style="width: 80px; height: 80px"
+          />
           <div class="content-wrapper">
             <div></div>
             <div></div>
             <div></div>
             <div>
-              <div class="icon-text-wrapper"><span class="iconfont prefix-icon"></span>
-                <span class="text"><p>{{ hospital.route }}</p>
-              </span><span class="iconfont right-icon"></span></div>
+              <div class="icon-text-wrapper">
+                <span class="iconfont prefix-icon"></span>
+                <span class="text"
+                  ><p>{{ hospital.route }}</p> </span
+                ><span class="iconfont right-icon"></span>
+              </div>
             </div>
           </div>
         </div>
-        <div class="title mt40"> 医院介绍</div>
-        <div class="detail-content mt40"><p>{{ hospital.intro }}</p></div>
+        <div class="title mt40">医院介绍</div>
+        <div class="detail-content mt40">
+          <p>{{ hospital.intro }}</p>
+        </div>
       </div>
     </div>
     <!-- 右侧内容 #end -->
@@ -51,35 +88,34 @@
 </template>
 
 <script>
-import '~/assets/css/hospital_personal.css'
-import '~/assets/css/hospital.css'
+import "~/assets/css/hospital_personal.css";
+import "~/assets/css/hospital.css";
 
-import hospitalApi from '@/api/hosp'
+import hospitalApi from "@/api/hosp";
 export default {
   data() {
     return {
       hoscode: null,
       hospital: {
-        param: {}
-      }
-    }
+        param: {},
+      },
+    };
   },
 
   created() {
-    this.hoscode = this.$route.params.hoscode
+    this.hoscode = this.$route.params.hoscode;
 
-    this.init()
+    this.init();
   },
 
   methods: {
     init() {
-      hospitalApi.show(this.hoscode).then(response => {
-        this.hospital = response.data.hospital
-      })
-
-    }
-  }
-}
+      hospitalApi.show(this.hoscode).then((response) => {
+        this.hospital = response.data.hospital;
+      });
+    },
+  },
+};
 </script>
 <style>
 .hospital-detail .info-wrapper {
@@ -92,7 +128,6 @@ export default {
 
 .hospital-detail .info-wrapper .text {
   font-size: 14px;
-
 }
 
 .hospital-detail .content-wrapper p {
