3bc8ff635676ea81f0373d27d29385f59bb84319
ZHI <891023883@qq.com>
2021-12-24 18:33:02 +0800

    预约挂号详情

--- api/hosp.js
+++ api/hosp.js
@@ -1,33 +1,45 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
-const api_name = `/api/hosp/hospital`
+const api_name = `/api/hosp/hospital`;
 
 export default {
   getPageList(page, limit, searchObj) {
     return request({
       url: `${api_name}/findHospList/${page}/${limit}`,
-      method: 'get',
-      params: searchObj
-    })
+      method: "get",
+      params: searchObj,
+    });
   },
 
   getByHosname(hosname) {
     return request({
       url: `${api_name}/findByHosName/${hosname}`,
-      method: 'get'
-    })
+      method: "get",
+    });
   },
   show(hoscode) {
     return request({
       url: `${api_name}/findHospDetail/${hoscode}`,
-      method: 'get'
-    })
+      method: "get",
+    });
   },
   findDepartment(hoscode) {
     return request({
       url: `${api_name}/department/${hoscode}`,
-      method: 'get'
-    })
-  }
+      method: "get",
+    });
+  },
+  getBookingScheduleRule(page, limit, hoscode, depcode) {
+    return request({
+      url: `${api_name}/auth/getBookingScheduleRule/${page}/${limit}/${hoscode}/${depcode}`,
+      method: "get",
+    });
+  },
 
-}
+  findScheduleList(hoscode, depcode, workDate) {
+    return request({
+      url: `${api_name}/auth/findScheduleList/${hoscode}/${depcode}/${workDate}`,
+      method: "get",
+    });
+  },
+};
