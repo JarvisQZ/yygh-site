287874f9a5425084e60c78d5261212f7362a0be8
ZHI <891023883@qq.com>
2021-12-25 02:27:44 +0800

    订单管理

--- pages/hospital/booking.vue
+++ pages/hospital/booking.vue
@@ -229,6 +229,7 @@ import "~/assets/css/hospital.css";
 
 import hospitalApi from "@/api/hosp";
 import patientApi from "@/api/patient";
+import orderInfoApi from "@/api/orderInfo";
 
 export default {
   data() {
@@ -278,14 +279,35 @@ export default {
       this.patient = this.patientList[index];
     },
 
-    submitOrder() {},
+    //生成订单
+    submitOrder() {
+      if (this.patient.id == null) {
+        this.$message.error("请选择就诊人");
+        return;
+      }
+      // 防止重复提交
+      if (this.submitBnt == "正在提交...") {
+        this.$message.error("重复提交");
+        return;
+      }
+
+      this.submitBnt = "正在提交...";
+      orderInfoApi
+        .submitOrder(this.scheduleId, this.patient.id)
+        .then((response) => {
+          let orderId = response.data;
+          window.location.href = "/order/show?orderId=" + orderId;
+        })
+        .catch((e) => {
+          this.submitBnt = "确认挂号";
+        });
+    },
 
     addPatient() {
       window.location.href = "/patient/add";
     },
   },
 };
-
 </script>
 <style>
 .hospital-order .header-wrapper {
